c926326f9babe21f6a477aa56569f8953025f287
Guilherme Fernandes <guilhermefs09@protonmail.com>
2024-04-17 21:32:24 -0300

    body-parser atualizado

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const axios = require("axios");
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json())
